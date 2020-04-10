ef6e087461108611c2e180d0a34b0241426b532e
Luis Guerrero <luis.guerrero@conekta.com>
2020-04-10 16:15:51 -0500

    Setup title per page

--- test/site_pages_test.rb
+++ test/site_pages_test.rb
@@ -3,6 +3,10 @@ require_relative 'test_helper'
 class PalindromeAppTest < Minitest::Test
   include Rack::Test::Methods
 
+  def setup
+    @base_title = "Palindrome App"
+  end
+
   def app
     Sinatra::Application
   end
@@ -11,17 +15,23 @@ class PalindromeAppTest < Minitest::Test
     get '/'
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
+    title_content = doc(last_response).at_css('title').content
+    assert_equal "Home | #{@base_title}", title_content
   end
 
   def test_about
     get '/about'
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
+    title_content = doc(last_response).at_css('title').content
+    assert_equal "About | #{@base_title}", title_content
   end
 
   def test_palindrome
     get '/palindrome'
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
+    title_content = doc(last_response).at_css('title').content
+    assert_equal "Palindrome Detector | #{@base_title}", title_content
   end
 end
